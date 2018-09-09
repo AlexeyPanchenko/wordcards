24b61cb46ae7c4b761b03f5fb7ba85e52649f24e
Alexey Panchenko <panchenko@farpost.com>
2018-09-09 17:02:55 +1000

    Add buildSrc groovy module.

---
+++ buildSrc/src/main/groovy/ru/alexeypan/wordcards/Deps.groovy
@@ -0,0 +1,18 @@
+package ru.alexeypan.wordcards
+
+class ApplicationId {
+  static final String id = "ru.alexeypan.wordcards"
+}
+
+class Release {
+  static final int versionCode = 1
+  static final String versionName = "1.0"
+}
+
+class Versions {
+  static final String androidPlugin = "3.1.4"
+
+  static final int compileSdk = 28
+  static final int minSdk = 15
+  static final int targetSdk = 28
+}
