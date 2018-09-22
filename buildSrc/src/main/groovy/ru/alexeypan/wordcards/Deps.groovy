2000e5f50e6a515076d515c327dd1dee28491b96
Alexey Panchenko <panchenko@farpost.com>
2018-09-22 16:03:23 +1000

    Select Word List feature module.

--- buildSrc/src/main/groovy/ru/alexeypan/wordcards/Deps.groovy
+++ buildSrc/src/main/groovy/ru/alexeypan/wordcards/Deps.groovy
@@ -4,6 +4,11 @@ class ApplicationId {
   static final String id = "ru.alexeypan.wordcards"
 }
 
+class Modules {
+  static final String wordlistApi = ":features-wordlist-api"
+  static final String wordlistImpl = ":features-wordlist-impl"
+}
+
 class Release {
   static final int versionCode = 1
   static final String versionName = "1.0"
@@ -15,4 +20,13 @@ class Versions {
   static final int compileSdk = 28
   static final int minSdk = 15
   static final int targetSdk = 28
+
+  static final String kotlin = "1.2.70"
+}
+
+class Libs {
+  static final String androidPlugin = "com.android.tools.build:gradle:${Versions.androidPlugin}"
+
+  static final String kotlinPlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
+  static final String kotlin = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}"
 }
