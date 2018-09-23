c577e817899c31a8b9cfafe8cdce5776ef48306b
Alexey Panchenko <panchenko@farpost.com>
2018-09-23 18:11:10 +1000

    AndroidX and Material 1.0.0. Define statusBar color in app theme.

--- buildSrc/src/main/groovy/ru/alexeypan/wordcards/Deps.groovy
+++ buildSrc/src/main/groovy/ru/alexeypan/wordcards/Deps.groovy
@@ -22,6 +22,12 @@ class Versions {
   static final int targetSdk = 28
 
   static final String kotlin = "1.2.70"
+  static final String androidx = "1.0.0"
+  static final String constraintLayout = "1.1.3"
+
+  static final String junit = "1.1.3"
+  static final String testRunner = "1.1.0-alpha3"
+  static final String espresso = "3.1.0-alpha4"
 }
 
 class Libs {
@@ -29,4 +35,21 @@ class Libs {
 
   static final String kotlinPlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
   static final String kotlin = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}"
+
+  static final String material = "com.google.android.material:material:$Versions.androidx"
+
+  static class Androidx {
+    static final String annotations = "androidx.annotation:annotation:$Versions.androidx"
+    static final String appcompat = "androidx.appcompat:appcompat:$Versions.androidx"
+    static final String recyclerView = "androidx.recyclerview:recyclerview:$Versions.androidx"
+    static final String constraintLayout = "androidx.constraintlayout:constraintlayout:$Versions.constraintLayout"
+  }
+
+  static class Test {
+    static final String junit = "junit:junit:$Versions.junit"
+    static final String testRunner = "androidx.test:runner:$Versions.testRunner"
+    static final String espresso = "androidx.test.espresso:espresso-core:$Versions.espresso"
+  }
 }
+
+
--- features/features-wordlist-impl/src/main/java/ru/alexeypan/wordcards/wordlist/impl/WordListActivity.kt
+++ features/features-wordlist-impl/src/main/java/ru/alexeypan/wordcards/wordlist/impl/WordListActivity.kt
@@ -1,12 +1,16 @@
 package ru.alexeypan.wordcards.wordlist.impl
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.word_list_activity.*
 
 class WordListActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.word_list_activity)
+    setSupportActionBar(bottomBar)
+    bottomBar.setNavigationOnClickListener { Toast.makeText(this, "REE", Toast.LENGTH_SHORT).show() }
   }
 }
