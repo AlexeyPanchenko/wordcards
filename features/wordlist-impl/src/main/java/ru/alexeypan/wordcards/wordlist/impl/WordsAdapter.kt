88c5a8e01e46993ce6f5e94126abb48eb439866d
Alexey Panchenko <panchenko@farpost.com>
2018-12-16 15:18:44 +1000

    Add flip animation for word cards.

--- features/wordlist-impl/src/main/java/ru/alexeypan/wordcards/wordlist/impl/WordsAdapter.kt
+++ features/wordlist-impl/src/main/java/ru/alexeypan/wordcards/wordlist/impl/WordsAdapter.kt
@@ -1,11 +1,16 @@
 package ru.alexeypan.wordcards.wordlist.impl
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.ObjectAnimator
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AccelerateInterpolator
+import android.view.animation.DecelerateInterpolator
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.card.MaterialCardView
 
 class WordsAdapter : RecyclerView.Adapter<WordHolder>() {
 
@@ -37,7 +42,19 @@ class WordsAdapter : RecyclerView.Adapter<WordHolder>() {
     val word = words[position]
     holder.wordView.text = if (word.state == WordState.ORIGINAL) word.original else word.translate
     holder.itemView.setOnClickListener {
-      clickListener?.invoke(word, position)
+      val animator1 = ObjectAnimator.ofFloat(holder.itemView, "rotationY", 0f, 90f)
+      val animator2 = ObjectAnimator.ofFloat(holder.itemView, "rotationY", -90f, 0f)
+      animator1.duration = 300
+      animator2.duration = 300
+      animator1.interpolator = AccelerateInterpolator()
+      animator2.interpolator = DecelerateInterpolator()
+      animator1.addListener(object : AnimatorListenerAdapter() {
+        override fun onAnimationEnd(animation: Animator?) {
+          clickListener?.invoke(word, position)
+          animator2.start()
+        }
+      })
+      animator1.start()
     }
   }
 
@@ -47,6 +64,11 @@ class WordsAdapter : RecyclerView.Adapter<WordHolder>() {
 }
 
 class WordHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-  val card: MaterialCardView = itemView.findViewById(R.id.cardWord)
+  val card: CardView = itemView.findViewById(R.id.cardWord)
   val wordView: TextView = itemView.findViewById(R.id.tvWord)
+
+  init {
+    val scale = itemView.resources.displayMetrics.density
+    itemView.cameraDistance = 8000 * scale
+  }
 }
